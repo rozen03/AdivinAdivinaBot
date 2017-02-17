d58a43b7374909280ea28ef8a2aa854b857a3a21
uriel <urielrozenberg@hotmail.com>
2017-02-17 15:45:03 -0300

    moved pupa and slightly modified

--- AdivinaAdvinaBot.py
+++ AdivinaAdvinaBot.py
@@ -10,7 +10,7 @@ from rozentools.commontools import *
 from rozentools.errortools import *
 from tookns import AdivinaAdivinaBottookn
 from random import shuffle
-
+NOTHING, STARTED, JOINED = range(2)
 def start(bot, update):
 	registrar(bot, update)
 	update.message.reply_text(text="Holas, soy el AdivinaAdivinaBot, Por que no te vas a la mierda? :D",quote=False)
@@ -28,29 +28,25 @@ def startGame(bot, update):
 	juego = Juego(grupo=grupo)
 	jugador = Jugador()
 
-def personaje(bot,update,groups):
-	user = getUser(update)
-	group = getGroup(update)
-	texto=groups[1]#Tomo el textoju7ju7ju7u89iui
-	i=0
-	while(i < len(juego)):
-		jugador,texto= juego[i]
-		if (jugador == user.id):
-			del (juego[i])
-			juego.append(jugador,texto)
-	tienenTexto= [texto is not None for jugador,texto in juego]
-	if (all(tienenTexto)):
+@db_session
+def join(bot, update):
+	usuario, grupo = registrar(bot, update)
+	jugador = Jugador.get(user=usuario,grupo=grupo)
+	if (jugador is None):
+		jugador = Jugador(usuario=usuario,grupo=grupo)
+	juego = Juego.get(grupo=grupo)
+	if (jugador not in juego.jugadores)
+		juego.jugadores.add(jugador)
+		return JOINED
+def iniciarJuego(usuario, grupo):
+	juego = Juego.get(grupo=grupo)
+	if (juego is None):
+		juego = Juego(grupo=grupo)
+	join(usuario, grupo)
 
-		lista = list(juego)			# ejemplo: lista = [(1, 'uno'), (2, 'dos'), (3, 'tres'), (4, 'cuatro')]
-		shuffle(lista)              # mezclo la lista aleatoriamente
-		lista = list(zip(*lista))   # traspongo la lista (obtengo 2 tuplas, una con los primeros elementos y otra con los segundos:
-		                            #   [(1, 2, 3, 4), ('uno', 'dos', 'tres', 'cuatro')])
-		l1, l2 = map(list, lista)   # convierto las tuplas en listas (aplicando la funcion list() a c/u de los elementos de lista)
-		primero = l1.pop(0)         # elimino el primer elemento de l1 y lo guardo
-		l1.append(primero)          # lo agrego al final (para shiftear l1 en un lugar)
-		juego = list(zip(l1, l2))   # convierto las dos listas en una sola
-		for jugador,texto in juego:
-			mandarMensaje(bot,jugador,str.join('\n',[jugador_+":"+texto_ in juego if jugador_ != jugador]))		
+def startGame(bot,update):
+	user, group = registrar(bot, update)
+
 
 #NOTA: Desde esta parte del codigo no le den mucha bola si quieren, esto inicializa un monton de cosas
 def main():
